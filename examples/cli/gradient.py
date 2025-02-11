09bc7085c58e5e98273ca6983b911b56b5a5c6cd
Tom MacWright <tom@macwright.com>
2025-02-10 19:11:20 -0500

    Remove another unused

--- examples/cli/gradient.py
+++ examples/cli/gradient.py
@@ -1,8 +1,6 @@
-import pytz
 from PIL import Image as ImagePIL
 from PIL import ImageDraw
 
-# import pytz
 from indiepixel import Box, Column, Rect, Row
 
 
@@ -11,7 +9,6 @@ def render():
     im = ImagePIL.new("RGB", (64, 32))
     draw = ImageDraw.Draw(im)
     draw.fontmode = "1"
-    tz = pytz.timezone("America/New_York")
     layout = Box(
         Column(
             children=[
