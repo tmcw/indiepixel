5bbcb75704001a620a6e9f3361a2fe35558e700c
Tom MacWright <tom@macwright.com>
2025-03-02 18:01:57 -0500

    Bind o all interface

--- src/indiepixel/cli.py
+++ src/indiepixel/cli.py
@@ -85,4 +85,4 @@ def cli(filename: str, duration: int):
     app = create_server(filename, duration)
     print("created app", app)
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, port=port, host="0.0.0.0")  # noqa: S104
